fac1ab07f230c525c517b110dbbc92578fed3eae
TAHZUDDIN AHMED <atahzuddin@yahoo.com>
2017-10-10 15:57:26 +0530

    in foodActivity dagger is added

--- app/src/main/java/com/raywenderlich/android/deezfoodz/components/AppComponent.java
+++ app/src/main/java/com/raywenderlich/android/deezfoodz/components/AppComponent.java
@@ -3,6 +3,7 @@ package com.raywenderlich.android.deezfoodz.components;
 import com.raywenderlich.android.deezfoodz.modules.AppModule;
 import com.raywenderlich.android.deezfoodz.modules.NetworkModule;
 import com.raywenderlich.android.deezfoodz.modules.PresenterModule;
+import com.raywenderlich.android.deezfoodz.ui.food.FoodActivity;
 import com.raywenderlich.android.deezfoodz.ui.food.FoodPresenterImpl;
 import com.raywenderlich.android.deezfoodz.ui.foodz.FoodzActivity;
 import com.raywenderlich.android.deezfoodz.ui.foodz.FoodzPresenterImpl;
@@ -19,6 +20,7 @@ import dagger.Component;
 @Component(modules = {AppModule.class, PresenterModule.class, NetworkModule.class})
 public interface AppComponent {
     void inject(FoodzActivity target);
+    void inject(FoodActivity target);
     void inject(FoodzPresenterImpl target);
     void inject(FoodPresenterImpl target);
 }
